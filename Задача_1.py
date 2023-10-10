7b938111d212b92b192723964d8a7f3edf05db99
Публичный профиль <143626776+Publichny@users.noreply.github.com>
2023-10-10 05:34:49 +0300

    Add files via upload

---
+++ Задача_1.py
@@ -0,0 +1,35 @@
+# print(1.13, 49)
+
+# print(7.16, 100, sep="   ")
+
+# print(1,2,3,4, sep=" ")
+
+# print(5, 10, 21, sep="\n")
+
+# print(1,2,3,4,sep="\n")
+
+# print("5 10", "7см", sep="\n")
+
+# print(14**5)
+
+# print(2**4, 2**6)
+
+# n = int(input("n-ni kiriting: "))
+# print(((2-3)*2)*n)
+
+
+
+
+
+
+
+# print("""Taqdir yuki boshimga balolig' bo'ldi,
+# Har neniki ayladim hatolig' bo'ldi,
+# Oz yurtni qo'yib Hind sori yuzlandim,
+# Yo Rab netayin, bu ne yuz qarolig' bo'ldi.""")
+
+# print("Study" +  "tonight")
+# print("Study", "tonight", sep=" | ")
+
+
+# print("Studytonight", "has", "been", "created", "for", "STUDENTS", sep=" _ ")
